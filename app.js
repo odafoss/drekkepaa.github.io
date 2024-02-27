eede2f1518cd963d231d1d0b2fea6b782b41a11f
odafoss <143173073+odafoss@users.noreply.github.com>
2024-02-27 18:30:25 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+
+// Funksjon som kjøres når en spillknapp klikkes
+function spillValgt(spillNavn) {
+    alert("Du valgte " + spillNavn);
+}
+
+// Legg til klikk-hendelseslytter for hver spillknapp
+document.getElementById("spill1").addEventListener("click", function () {
+    spillValgt("Spill 1");
+});
+
+document.getElementById("spill2").addEventListener("click", function () {
+    spillValgt("Spill 2");
+});
+
+
+
