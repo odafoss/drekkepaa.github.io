43eca125f16a23f7759d1eaf823ba745cbdfd1a2
odafoss <143173073+odafoss@users.noreply.github.com>
2024-02-28 14:32:17 +0100

    hs

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
 
-// Funksjon som kjøres når en spillknapp klikkes
-function spillValgt(spillNavn) {
-    alert("Du valgte " + spillNavn);
-}
-
-// Legg til klikk-hendelseslytter for hver spillknapp
-document.getElementById("spill1").addEventListener("click", function () {
-    spillValgt("Spill 1");
-});
-
-document.getElementById("spill2").addEventListener("click", function () {
-    spillValgt("Spill 2");
-});
-
-
 
 
